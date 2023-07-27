ad3aa9fc750e038ba5261217b43c5b23126a0982
OwenB23 <owenbond23@gmail.com>
2023-07-27 15:36:26 -0400

    Bug fixes and UI updates for bank app

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 // Data
 const account1 = {
   owner: "Cameron Bond",
-  movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
+  movements: [200, 4450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
   pin: 1111,
 };
@@ -17,14 +17,14 @@ const account2 = {
 
 const account3 = {
   owner: "Owen Bond",
-  movements: [200, -200, 340, -300, -20, 50, 400, -460],
+  movements: [200, -200, 340, -300, -20, 5000, 400, -460],
   interestRate: 0.7,
   pin: 3333,
 };
 
 const account4 = {
   owner: "Belinda Bond",
-  movements: [430, 1000, 700, 50, 90],
+  movements: [430, 100, 7700, 50, 900],
   interestRate: 1,
   pin: 4444,
 };
@@ -42,6 +42,7 @@ const labelTimer = document.querySelector(".timer");
 
 const containerApp = document.querySelector(".app");
 const containerMovements = document.querySelector(".movements");
+const test = document.querySelector(".test");
 
 const btnLogin = document.querySelector(".login__btn");
 const btnTransfer = document.querySelector(".form__btn--transfer");
@@ -58,29 +59,31 @@ const inputCloseUsername = document.querySelector(".form__input--user");
 const inputClosePin = document.querySelector(".form__input--pin");
 
 // DISPLAY MOVEMENTS
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = "";
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? "deposit" : "withdrawal";
 
     const html = `
-    <div class="movements__row">
-      <div class="movements__type movements__type--${type} ">${
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
       i + 1
     } ${type}</div>
-      <div class="movements__value">$${mov}</div>
-    </div>`;
+        <div class="movements__value">$${mov}</div>
+      </div>
+    `;
 
     containerMovements.insertAdjacentHTML("afterbegin", html);
   });
 };
-
 // DISPLAY BALANCE
 const calcDisplayBalance = function (acc) {
   acc.balance = acc.movements.reduce((acc, mov) => acc + mov, 0);
 
-  labelBalance.textContent = `$${acc.balance}`;
+  labelBalance.textContent = `$${acc.balance.toLocaleString()}`;
 };
 
 const calcDisplaySummary = function (account) {
@@ -101,7 +104,7 @@ const calcDisplaySummary = function (account) {
       return int >= 1;
     })
     .reduce((acc, int) => acc + int, 0);
-  labelSumInterest.textContent = `$${interest}`;
+  labelSumInterest.textContent = `$${interest.toFixed(1)}`;
 };
 
 // DEFINE USERNAMES FROM ACCOUNTS
@@ -145,6 +148,7 @@ btnLogin.addEventListener("click", function (e) {
       currentAccount.owner.split(" ")[0]
     }`;
     containerApp.style.opacity = 100;
+    test.style.display = "none";
 
     // Clear the Input Field
     inputLoginUsername.value = inputLoginPin.value = "";
@@ -174,4 +178,55 @@ btnTransfer.addEventListener("click", function (e) {
     receiverAcc.movements.push(amount);
     updateUI(currentAccount);
   }
+});
+
+btnLoan.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if (
+    amount > 0 &&
+    currentAccount.movements.some((mov) => mov >= amount * 0.1)
+  ) {
+    // Add movement
+    currentAccount.movements.push(amount);
+
+    // Update UI
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = "";
+});
+
+btnClose.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (
+    inputCloseUsername.value === currentAccount.username &&
+    Number(inputClosePin.value) === currentAccount.pin
+  ) {
+    const index = accounts.findIndex(
+      (acc) => acc.username === currentAccount.username
+    );
+    console.log(index);
+    // .indexOf(23)
+
+    // Delete account
+    accounts.splice(index, 1);
+
+    // Hide UI
+    containerApp.style.opacity = 0;
+  }
+  test.style.display = "flex";
+
+  labelWelcome.textContent = "Log in to get started";
+
+  inputCloseUsername.value = inputClosePin.value = "";
+});
+
+let sorted = false;
+btnSort.addEventListener("click", function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
